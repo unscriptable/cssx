8125441586b31cc1bb3574b1264dd6bef486e486
Kris Zyp <kriszyp@gmail.com>
2011-05-20 17:49:53 -0600

    Improved CSS to JS conversion

--- widget.js
+++ widget.js
@@ -1,32 +1,56 @@
 define(["./elemental"], function(elemental){
+	var evaluate = eval;
+	function cssToJs(value){
+		// recursively convert CSS to a reasonable JS equivalent
+		if(typeof value == "string"){
+			return evaluate(value);
+		}
+		var result, children = value.children;
+		if(children){
+			result = [];
+			for(var i = 0; i < children.length; i++){
+				result.push(cssToJs(children[i]));
+			}
+		}else{
+			result = {};
+		}
+		value.eachProperty(function(full, name, value){
+			result[name] = cssToJs(value);
+		});
+		return result;
+	}
 	return elemental.extend({
 		onwidget: function(name, value, rule){
-			var modules = [];
-			value.replace(/require\s*\(\s*['"]([^'"]*)['"]\s*\)/g, function(t, moduleId){
-				modules.push(moduleId);
-			});
-			require(modules);
-			this.addRenderer(rule.selector, function(domNode){
-				require(modules, function(){
-					var __module = eval(value);
-					var prototype = __module.prototype;
-					var props = {};
-					if(prototype){
-						rule.eachProperty(function(t, name, value){
-							if(name in prototype){
-								var type = typeof prototype[name];
-								if(type == "string" || typeof value != "string"){
-									props[name] = value;
-								}else if(type == "number"){
-									props[name] = +value;
-								}else{
-									props[name] = eval(value);
-								}
+			var __module = eval(value);
+			var prototype = __module.prototype;
+			var props = {};
+			if(prototype){
+				rule.eachProperty(function(t, name, value){
+					// change from css-style to javaScript style names
+					name = name.replace(/-[a-z]/g, function(match){
+						return match[1].toUpperCase();
+					});
+					// copy properties over
+					if(name in prototype){
+						var type = typeof prototype[name];
+						if(type == "number"){
+							value = +value;
+						}else if(type != "string"){
+							if(prototype['_' + name + 'Css']){
+								value = prototype['_' + name + 'Css'](value);
+							}else{
+								value = cssToJs(value);
 							}
-						});
+						}
+						props[name] = value;
 					}
-					__module(props, domNode);
 				});
+			}
+			function Props(){}
+			Props.prototype = props;
+			this.addRenderer(rule, function(domNode){
+				// instantiate widget
+				__module(new Props, domNode);
 			});
 		},
 		role: "layout"
