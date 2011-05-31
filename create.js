fb936b0b58feec4c9a463ab074a53fce9d3dcb63
Kris Zyp <kriszyp@gmail.com>
2011-05-31 14:47:28 -0600

    More cleanup and tests

--- create.js
+++ create.js
@@ -4,59 +4,54 @@ define([], function(){
 	//		cssx/create
 	// summary:
 	//		This module defines a fast lightweight function for creating new elements
-	//		terse, selector-based syntax. The single return is a function that creates
-	// 		new DOM elements with the signature
-	// 		create(parent?, elementName or array of children...);
-	//		The first argument, parent, is optional, and can be an element to append the newly
-	//   	elements to.
-	//		All subsequent arguments are used to create new elements. An argument can
-	//		be a string, in which case it is interpreted with the same syntax as a CSS 
-	// 		selector. Tag syntax (no prefix) is used to indicate the tag to be created,
+	//		terse, CSS selector-based syntax. The single function from this module creates
+	// 		new DOM elements with the signature:
+	// 		create(referenceElement?, selector, properties|innerHTML);
+	//		The first argument, referenceElement, is optional, and is the reference element
+	//		for the selector. Tag syntax (no prefix) is used to indicate the tag to be created,
 	//		.class-name can be used to assign the class name, #id can be used to assign an id.
 	//		and [name=value] can be used to assign additional attributes to the element.
 	// 		The attribute assignment will always use setAttribute to assign the attribute to the element.  
-	//		For example, "div.my-class" would create <div> element with a class of "my-class".
-	//		CSS combinators can be used to create child elements and sibling elements. 
-	//		There can be an unlimited number of arguments, each creating new elements
-	//		(and appending to the parent if provided). 
+	//		For example, create("div.my-class") would create <div> element with a class of "my-class".
+	//		(and appending to the referenceElement if provided). 
+	//		CSS combinators can be used to create child elements and sibling elements.
 	//		The create function returns the last top level element created or referenced (by a suffix combinator).
 	//	examples:
-	//		To create a 
-	//		To create a table as child of the parent, one could write:
-	//		create(parent, "table.class-name#id tr.class-name td[colSpan=2]<tr.class-name td+td<<");
+	//		To create a simple div:
+	//		|	create("div");
+	//		To create a table as child of the referenceElement, one could write:
+	//		create(referenceElement, "table.class-name#id tr.class-name td[colSpan=2]<tr.class-name td+td<<");
 	//
-	//		create(parent, "table.class-name#id tr.class-name");
+	//		create(referenceElement, "table.class-name#id tr.class-name");
 					
-	var selectorParse = /(([-+])|[,<> ])?\s*(\.|#)?([-\w]+)?(?:\[([^\]=]+)=['"]?([^\]'"]+)['"]?\])?/g,
+	var selectorParse = /(([-+])|[,<> ])?\s*(\.|#)?([-\w]+)?(?:\[([^\]=]+)=?['"]?([^\]'"]*)['"]?\])?/g,
 		className = "className";		
-	function create(parent, selector, properties){
-		if(typeof parent == "string"){
+	function create(referenceElement, selector, properties){
+		if(typeof referenceElement == "string"){
 			// first parameter is optional,
-			if(typeof selector == "object"){
-				properties = selector;
-			} 
-			selector = parent;
-			parent = null;
+			properties = selector;
+			selector = referenceElement;
+			referenceElement = null;
 		}
-		var nextSibling = null, current, topParent = parent;
+		var nextSibling = null, current, topReferenceElement = referenceElement;
 		var leftoverCharacters = selector.replace(selectorParse, function(t, combinator, siblingCombinator, prefix, value, attrName, attrValue){
 			if(combinator){
 				if(siblingCombinator){
 					// + or - combinator, 
-					parent = (nextSibling = (current || parent)).parentNode;
+					referenceElement = (nextSibling = (current || referenceElement)).parentNode;
 					if(siblingCombinator == "+"){
 						nextSibling = nextSibling.nextSibling;
-					}
+					}// else a - operator, again not in CSS, but obvious in it's meaning (create next element before the current/referenceElement)
 				}else{
 					if(combinator == "<"){
 						// parent combinator (not really in CSS, but theorized, and obvious in it's meaning)
-						current = (current || parent).parentNode;
+						current = (current || referenceElement).parentNode;
 					}else if(combinator == ","){
 						// comma combinator, start a new selector
-						current = topParent;
+						current = topReferenceElement;
 					}
 					// else descendent or child selector (doesn't matter, but treated the same),
-					parent = current;
+					referenceElement = current;
 					current = null;
 					nextSibling = null;
 				}
@@ -65,8 +60,8 @@ define([], function(){
 			if(tag || (!current && (prefix || attrName))){
 				// Need to create an element
 				current = document.createElement(tag || create.defaultTag);
-				if(parent){
-					parent.insertBefore(current, nextSibling);
+				if(referenceElement){
+					referenceElement.insertBefore(current, nextSibling);
 				}
 			}
 			if(prefix){
@@ -80,17 +75,22 @@ define([], function(){
 			}
 			if(attrName){
 				// [name=value]
-				current.setAttribute(attrName, attrValue);
+				current.setAttribute(attrName, attrValue || attrName);
 			}
 			return '';
 		});
 		if(leftoverCharacters){
 			throw new SyntaxError("Unexpected char " + leftoverCharacters);
 		}
-		for(var i in properties){
-			current[i] = properties[i];
+		current = current || referenceElement;
+		if(typeof properties == "string"){
+			current.innerHTML = properties;
+		}else{
+			for(var i in properties){
+				current[i] = properties[i];
+			}
 		}
-		return current || parent;
+		return current;
 	}
 	create.defaultTag = "div";
 	return create;
--- test/create.js
+++ test/create.js
@@ -2,7 +2,7 @@ var div = create("div");
 console.assert(div.tagName.toLowerCase() == "div");
 
 var body = document.body;
-create(body, "h1", {innerHTML:"Running create() tests"});
+create(body, "h1", "Running create() tests");
 
 var parent = div;
 
@@ -13,7 +13,7 @@ console.assert(span1.parentNode == div);
 
 var defaultTag = create(parent, ".class");
 console.assert(defaultTag.tagName.toLowerCase() == "div");
-var span3 = create(span1, "+span[name=span2]+ span[name=span3]");
+var span3 = create(span1, "+span[name=span2] + span[name=span3]");
 console.assert(span3.getAttribute("name") == "span3");
 console.assert(span3.previousSibling.getAttribute("name") == "span2");
 console.assert(span3.previousSibling.previousSibling.getAttribute("name") == "span1");
@@ -32,9 +32,12 @@ console.assert(table.firstChild.childNodes.length == 1);
 console.assert(table.lastChild.className == "class-name");
 console.assert(table.lastChild.childNodes.length == 2);
 
-create(table, "tr td,tr td+td");
+create(table, "tr>td,tr>td+td");
 console.assert(table.childNodes.length == 4);
 console.assert(table.lastChild.childNodes.length == 2);
 
+var checkbox = create(div, "input[type=checkbox][checked]");
+console.assert(checkbox.type == "checkbox");
+console.assert(checkbox.getAttribute("checked") == "checked");
 
 create(body, "div", {innerHTML: "finished tests, check console for errors"});
